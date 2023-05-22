5e109198591850c80a44eefc784fcb2996b92a31
Philip Jambrisak <jambrisak@gmail.com>
2023-05-22 20:40:15 +0200

    Design
    Slight design updates.

--- bowling-score-calculator/src/components/BowlingCalculator.js
+++ bowling-score-calculator/src/components/BowlingCalculator.js
@@ -12,24 +12,24 @@ const BowlingCalculator = () => {
       setGameOver(true);
       return;
     }
-  
+
     const currentFrame = frames[currentFrameIndex];
     const previousRoll = currentFrame[currentRoll - 1];
-  
+
     // Validate the input to prevent more than 10 pins knocked down in a frame
     if (currentRoll === 1 && previousRoll !== 10 && previousRoll + pins > 10) {
       return;
     }
-  
+
     setFrames((prevFrames) => {
       const updatedFrames = [...prevFrames];
       const frameCopy = [...updatedFrames[currentFrameIndex]];
       frameCopy[currentRoll] = pins;
       updatedFrames[currentFrameIndex] = frameCopy;
-  
+
       return updatedFrames;
     });
-  
+
     // Handle logic for advancing to the next frame or roll
     if (currentFrameIndex === 9) {
       if (currentRoll === 0 && pins === 10) {
@@ -48,7 +48,6 @@ const BowlingCalculator = () => {
       setCurrentRoll((prevRoll) => prevRoll + 1);
     }
   };
-  
 
   const renderRoll = (roll) => {
     if (roll === 10) {
@@ -64,7 +63,7 @@ const BowlingCalculator = () => {
     const frame = frames[frameIndex];
     const isStrike = frame[0] === 10;
     const isSpare = frame[0] + frame[1] === 10;
-  
+
     if (isStrike) {
       if (frameIndex < 9) {
         const nextFrame = frames[frameIndex + 1];
@@ -78,20 +77,27 @@ const BowlingCalculator = () => {
         return 10 + frame[1] + frame[2];
       }
     }
-  
+
     if (isSpare && frameIndex < 9) {
       const nextFrame = frames[frameIndex + 1];
       return 10 + nextFrame[0];
     }
-  
+
     return frame[0] + frame[1];
   };
 
   const calculateOverallScore = () => {
     let score = 0;
+
     for (let i = 0; i < frames.length; i++) {
-      score += calculateFrameScore(i);
+      score += i === 9 ? calculateFrameScore(i) || 0 : calculateFrameScore(i);
     }
+
+    // Handle the case of a perfect game (12 strikes)
+    if (score === 270) {
+      score = 300;
+    }
+
     return score;
   };
 
@@ -107,28 +113,30 @@ const BowlingCalculator = () => {
       <h1>Bowling Score Calculator</h1>
 
       <div className="scorecard">
-        {frames.map((frame, frameIndex) => (
-          <div key={frameIndex} className="frame">
-            <div className="frame-index">{frameIndex + 1}</div>
-            <div className="rolls">
-              {frame.map((roll, rollIndex) => (
-                <div key={rollIndex} className="roll">
-                  {renderRoll(roll)}
-                </div>
-              ))}
-              {frameIndex === 9 && frame.length < 3 && (
-                <div className="roll">
-                  {frame.length === 1 ? '-' : renderRoll(frame[1])}
-                </div>
-              )}
-            </div>
+  {frames.map((frame, frameIndex) => (
+    <div
+      key={frameIndex}
+      className={`frame ${frameIndex === currentFrameIndex ? 'current-frame' : ''}`}
+    >
+      <div className="frame-index">{frameIndex + 1}</div>
+      <div className="rolls">
+        {frame.map((roll, rollIndex) => (
+          <div key={rollIndex} className="roll">
+            {renderRoll(roll)}
           </div>
         ))}
+        {frameIndex === 9 && frame.length < 3 && (
+          <div className="roll">{frame.length === 1 ? '-' : renderRoll(frame[1])}</div>
+        )}
+        {frameIndex === 9 && frame.length === 3 && (
+          <div className="roll">{renderRoll(frame[2])}</div>
+        )}
       </div>
+    </div>
+  ))}
+</div>
 
-      <div className="overall-score">
-        Overall Score: {calculateOverallScore()}
-      </div>
+      <div className="overall-score">Overall Score: {calculateOverallScore()}</div>
 
       <div className="input-container">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((pins) => (
